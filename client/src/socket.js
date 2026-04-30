05876e62dc2c3a9e3d13e68c6332723d87aaf8d7
Alexandre <alexandreoger35680@gmail.com>
2026-04-30 11:22:10 +0200

    transport socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,8 @@
 import io from 'socket.io-client';
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3001';
-const socket = io(SERVER_URL);
+const socket = io(SERVER_URL, {
+  transports: ['websocket']
+});
 
 export default socket;
